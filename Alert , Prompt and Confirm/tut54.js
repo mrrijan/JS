0d483adf63fa81db67c291c618c65a3a6a119db8
mrrijan <102971085+mrrijan@users.noreply.github.com>
2023-03-04 20:39:37 +0545

    Add files via upload

---
+++ Alert , Prompt and Confirm/tut54.js
@@ -0,0 +1,27 @@
+
+//ALert in in-browser javascript (Note:alert does not return anything)
+// alert("You have to allow cookies");
+
+//prompt in js
+// let name=prompt("What is your name ? ","Guest");
+// console.log(name);
+
+//Confirm in JS
+// let deletepost=confirm("Do you really want to delete this post ? ");
+// // console.log(deletepost);
+// if(deletepost){
+//     console.log("Your post has been deleted succesfully");
+// }
+// else{
+//     console.log("Your post was not deleted");
+// }
+
+//Quick Quiz
+let age =prompt("Enter your age ");
+if(age>18)
+{
+    alert("You are allowed to enter");
+}
+else{
+    alert("You are not allowed here ");
+}
